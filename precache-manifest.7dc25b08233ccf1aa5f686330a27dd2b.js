5277f4065c0e421ec04480e53c49a891da751421
vitaminvp <vitamin@ukr.net>
2019-08-25 22:27:23 +0300

    Updates

---
+++ precache-manifest.7dc25b08233ccf1aa5f686330a27dd2b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "627c577295b0f82df362eda48a8e1bc7",
+    "url": "/forms-cra-spa/index.html"
+  },
+  {
+    "revision": "27f7723ea7e1824bb3b1",
+    "url": "/forms-cra-spa/static/css/main.4845f556.chunk.css"
+  },
+  {
+    "revision": "970e3bd4f9d232b82fdb",
+    "url": "/forms-cra-spa/static/js/2.a3689a4d.chunk.js"
+  },
+  {
+    "revision": "27f7723ea7e1824bb3b1",
+    "url": "/forms-cra-spa/static/js/main.62310a3d.chunk.js"
+  },
+  {
+    "revision": "13c5682329319e3fecb9",
+    "url": "/forms-cra-spa/static/js/runtime~main.d61ae950.js"
+  },
+  {
+    "revision": "d910f2c2394e8aa85e768c10812adc36",
+    "url": "/forms-cra-spa/static/media/avatar.d910f2c2.svg"
+  }
+]);
